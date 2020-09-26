7ee8fd314b3ccdeecb8240886a19d0a3b9502e0f
Sekar <sekar.n@appandgeek.com>
2020-09-26 11:23:22 +0530

    New route added

--- src/components/Layout/Main.js
+++ src/components/Layout/Main.js
@@ -2,9 +2,10 @@ import React, { Component } from "react";
 import { Router, Route } from "react-router-dom";
 import Login from "../../components/Login";
 import Dashboard from "../../components/Dashboard";
+import Temp from "../../components/Temp";
 import Organization from "../../components/Organization";
 import { history } from "../../helpers/history";
-import PrivateRoute from "../../helpers/privateRoute";
+// import PrivateRoute from "../../helpers/privateRoute";
 import ErrorBoundary from "../Utilities/ErrorBoundary";
 
 class RouterApp extends Component {
@@ -12,8 +13,9 @@ class RouterApp extends Component {
     return (
       <Router history={history}>
         <ErrorBoundary>
-          <PrivateRoute exact path="/organizations" component={Organization} />
-          <PrivateRoute exact path="/" component={Dashboard} />
+          <Route exact path="/organizations" component={Organization} />
+          <Route exact path="/" component={Dashboard} />
+          <Route exact path="/temp" component={Temp} />
           <Route exact path="/login" component={Login} />
         </ErrorBoundary>
       </Router>
