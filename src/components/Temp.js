acbc62dfff56afaffe50e85cc9c59197f424af4b
Sekar <sekar.n@appandgeek.com>
2020-09-26 11:41:14 +0530

    Temp file added

---
+++ src/components/Temp.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Card } from "antd";
+class Dashbpard extends React.Component {
+  state = {};
+
+  render() {
+    return (
+      <div>
+        <Card className="mb-4" title={"Title"}>
+          Temp
+        </Card>
+      </div>
+    );
+  }
+}
+
+export default Dashbpard;
