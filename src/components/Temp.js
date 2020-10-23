b147aa43adcf4c3ac0f69b925914757f4b04a3a8
sarvesh <pixelsaints@gmail.com>
2020-10-23 15:59:54 +0530

    Dropdow added on temp page

--- src/components/Temp.js
+++ src/components/Temp.js
@@ -1,13 +1,46 @@
 import React from "react";
-import { Card } from "antd";
+import { Card, Dropdown, Menu, Button, Icon, Upload } from "antd";
+import MenuItem from "antd/lib/menu/MenuItem";
 class Dashbpard extends React.Component {
   state = {};
 
+  cardTitle = (
+    <div className="d-flex align-items-center">
+      <h3 className="text-dark mb-0">Scenarios</h3>
+      <div className="ml-auto d-flex align-items-center">
+
+        <div>
+          <Dropdown overlay={
+            <Menu>
+              <Menu.Item className="py-3">
+                <Upload>
+                  <Button type="link" className="p-0">
+                    <Icon type="download" /> Export Data
+                  </Button>
+                </Upload>
+              </Menu.Item>
+              <Menu.Item className="py-3">
+                <Upload>
+                  <Button type="link" className="p-0">
+                    <Icon type="upload" /> Import Data
+                  </Button>
+                </Upload>
+              </Menu.Item>
+            </Menu>
+          } placement="bottomRight">
+            <Button className="px-2">Actions</Button>
+          </Dropdown>
+        </div>
+      </div>
+    </div>
+  );
+
   render() {
+
     return (
-      <div>
-        <Card className="mb-4" title={"Title"}>
-          Temp
+      <div className="outer-wrapper">
+        <Card title={this.cardTitle}>
+
         </Card>
       </div>
     );
